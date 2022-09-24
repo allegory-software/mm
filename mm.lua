35677a39ba28d4b3abcb7f47e2ffb24940caba66
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2022-09-24 19:45:03 +0300

    unimportant

--- mm.lua
+++ mm.lua
@@ -313,7 +313,7 @@ cmd('install [forealz]', 'Install or migrate mm', function(opt, doit)
 		insert_or_update_row('tenant', {
 			tenant = 1,
 			name = 'test',
-			host = config'domain',
+			host = config'host',
 		})
 		usr_create_or_update{
 			tenant = 1,
